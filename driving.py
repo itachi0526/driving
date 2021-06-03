ca4fc6141a049bdeef22b85a4114262b3b3923ee
Eric Haung <fiser0317@gmail.com>
2021-06-03 09:55:46 +0800

    add naruto

--- driving.py
+++ driving.py
@@ -5,4 +5,9 @@ if country == '台灣':
     if age < 18:
         print('你還太嫩了')
     else:
-        print('恭喜你可以開車車了')  
+        print('恭喜你可以開車車了')
+if country == '木葉忍者村':
+    if age > 30:
+        print('恭喜你，你活得比第一代火影那時的國名平均壽命還久')
+    else:
+        print('我愚蠢的歐豆豆呦')                     
