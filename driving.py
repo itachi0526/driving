1b78f03d569918acf3df6fe62820b02a2f1081a0
Eric Haung <fiser0317@gmail.com>
2021-06-03 10:08:12 +0800

    version3

--- driving.py
+++ driving.py
@@ -6,8 +6,10 @@ if country == '台灣':
         print('你還太嫩了')
     else:
         print('恭喜你可以開車車了')
-if country == '木葉忍者村':
+elif country == '木葉忍者村':
     if age > 30:
         print('恭喜你，你活得比第一代火影那時的國名平均壽命還久')
     else:
-        print('我愚蠢的歐豆豆呦')                     
+        print('我愚蠢的歐豆豆呦')
+else:
+    print('笑死，你只能是台灣人or木葉忍者村的')                            
